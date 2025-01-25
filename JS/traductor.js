e6a050e55ddbaaa8020f8da794592dfe21281436
DooS2003 <Lepal123@hotmail.com>
2025-01-25 12:12:50 -0500

    Cambios pagina

--- JS/traductor.js
+++
@@ -1,6 +0,0 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-      { pageLanguage: 'es', includedLanguages: 'en,fr,de,es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
-      'google_translate_element'
-  );
-}
